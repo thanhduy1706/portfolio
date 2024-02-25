b7c6635ebed5070abbc254406d4db6df73288007
Nguyen Duy <thanhduy1706@gmail.com>
2024-02-25 12:17:53 +0700

    Refactor sendEmail function to include server-side validation and use Resend library for sending emails

--- action/sendEmail.ts
+++ action/sendEmail.ts
@@ -1,33 +1,49 @@
 "use server";
 
+import React from "react";
 import { Resend } from "resend";
+import { validateString, getErrorMessage } from "@/lib/utils";
+import ContactFormEmail from "@/email/contact-form-email";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-const sendEmail = async (formData: FormData) => {
-  //   console.log("On server");
-  //   console.log(formData.get("senderEmail"));
-  //   console.log(formData.get("message"));
-
+export const sendEmail = async (formData: FormData) => {
   const senderEmail = formData.get("senderEmail");
   const message = formData.get("message");
 
-  if (
-    !senderEmail ||
-    !message ||
-    typeof senderEmail !== "string" ||
-    typeof message !== "string"
-  ) {
+  console.log(senderEmail, message);
+
+  // simple server-side validation
+  if (!validateString(senderEmail, 500)) {
     return {
-      error: "Please fill in all fields",
+      error: "Invalid sender email",
+    };
+  }
+  if (!validateString(message, 5000)) {
+    return {
+      error: "Invalid message",
     };
   }
 
-  resend.emails.send({
-    from: "onboarding@resend.dev",
-    to: "ndtduy.dev@gmail.com",
-    subject: "New message from your portfolio",
-    reply_to: senderEmail,
-    text: `From: ${formData.get("senderEmail")}\n\n${formData.get("message")}`,
-  });
+  let data;
+  try {
+    data = await resend.emails.send({
+      from: "Portfolio <onboarding@resend.dev>",
+      to: "ndtduy.work@gmail.com",
+      subject: "Message from contact form",
+      reply_to: senderEmail,
+      react: React.createElement(ContactFormEmail, {
+        message: message,
+        senderEmail: senderEmail,
+      }),
+    });
+  } catch (error: unknown) {
+    return {
+      error: getErrorMessage(error),
+    };
+  }
+
+  return {
+    data,
+  };
 };
