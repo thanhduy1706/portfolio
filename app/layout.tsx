3ab2727e321967a2a5fc074912af6567307228aa
Nguyen Duy <thanhduy1706@gmail.com>
2024-02-29 00:16:34 +0700

    Update colors and sizes in CSS and intro component

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,9 +20,9 @@ export default function RootLayout({
       <body
         className={`${inter.className} bg-gray-50 text-gray-950 relative h-[5000px] pt-28 sm:pt-36`}
       >
-        <div className="bg-[#4581f16c] absolute -z-10 top-[-6rem] right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]"></div>
+        <div className="bg-[#4581f154] absolute -z-10 top-[-6rem] right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]"></div>
 
-        <div className="bg-[#bd6cf3a9] absolute -z-10 top-[-1rem] left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"></div>
+        <div className="bg-[#bd6cf37c] absolute -z-10 top-[-1rem] left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"></div>
         <SpeedInsights />
         <ActiveSectionContextProvider>
           <Header />
--- components/intro.tsx
+++ components/intro.tsx
@@ -44,11 +44,11 @@ export default function Intro() {
             <Image
               src="https://i.scdn.co/image/ab6761610000e5ebd95cf4457fac4cc62311f84f"
               alt="Jennie portrait"
-              width="192"
-              height="192"
+              width="162"
+              height="162"
               quality="100"
               priority={true}
-              className="h-24 w-24 rounded-full object-cover border-[0.35rem] border-white shadow-xl"
+              className="h-29 w-29 rounded-full object-cover border-[0.35rem] hover:scale-110 transition border-white shadow-xl"
             />
           </motion.div>
 
@@ -63,7 +63,7 @@ export default function Intro() {
               duration: 0.7,
             }}
           >
-            👋
+            💻
           </motion.span>
         </div>
       </div>
@@ -80,7 +80,7 @@ export default function Intro() {
         }}
       >
         <span className="font-bold">Hi! I'm Duy.</span> I'm a{" "}
-        <span className="font-bold"> developer</span> with{" "}
+        <span className="font-bold"> junior</span> with{" "}
         <span className="font-bold">36 years</span> of experience. I enjoy
         building <span className="italic">sites & apps</span>. My focus is{" "}
         <span className="underline">React (Next.js)</span>.
