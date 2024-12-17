87268f8f8b42bae9e4fb6aae643a42bf9830ae9b
Nguyen Duy <thanhduy1706@gmail.com>
2024-12-17 20:41:47 +0700

    Add border radius to experience component and comment out icon styles

--- components/experience.tsx
+++ components/experience.tsx
@@ -28,6 +28,7 @@ export default function Experience() {
                 theme === "light" ? "#f3f4f6" : "rgba(255, 255, 255, 0.05)",
               boxShadow: "none",
               border: "1px solid rgba(0, 0, 0, 0.05)",
+              borderRadius: "1rem",
               textAlign: "left",
               padding: "1.3rem 2rem",
             }}
@@ -39,13 +40,13 @@ export default function Experience() {
             }}
             date={experience.date}
             icon={experience.icon}
-            iconStyle={{
-              background:
-                theme === "light" ? "white" : "rgba(255, 255, 255, 0.15)",
-              fontSize: "1.75rem",
-              boxShadow: "0 4px 6px rgba(0, 0, 0, 0.1)",
-              border: "1px solid #d1d5db",
-            }}
+            // iconStyle={{
+            //   background:
+            //     theme === "light" ? "white" : "rgba(255, 255, 255, 0.15)",
+            //   fontSize: "1.75rem",
+            //   boxShadow: "0 4px 6px rgba(0, 0, 0, 0.1)",
+            //   border: "1px solid #d1d5db",
+            // }}
           >
             <h3 className="font-bold text-xl text-gray-900 dark:text-white/95">
               {experience.title}
