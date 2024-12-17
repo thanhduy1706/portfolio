a0c5b93c071b7251f02e1a5ed70a7f2943930f5d
Duy Nguyen <thanhduy1706@gmail.com>
2024-12-17 15:48:20 +0700

    Update footer copyright year and improve website description formatting

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,11 +4,12 @@ export default function Footer() {
   return (
     <footer className="px-4 mb-10 text-center text-gray-500">
       <small className="block mb-2 text-xs">
-        &copy; 2024 Nguyen Dang Thanh Duy. All rights reserved.
+        &copy; 2025 Nguyen Dang Thanh Duy. All rights reserved.
       </small>
       <p className="text-xs">
         <span className="font-semibold">About this website:</span> built with
-        React (Next.JS), TypeScript, TailwindCSS, Framer Motion, Vercel hosting.
+        React (Next.JS), TypeScript, TailwindCSS, Framer Motion, and Vercel
+        hosting.
       </p>
     </footer>
   );
